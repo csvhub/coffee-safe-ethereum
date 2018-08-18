617ccdcd99b8ac4613c794bc1de47067c94af4c5
truongvanha <hachange.sgroup@gmail.com>
2018-08-18 19:18:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Coffee Tracking - The project of Happy City at Nami Hackathon
+
+Technologies:
+1. webpack
+2. truffle
+3. node
+
+## Running
+
+1. First run `truffle compile`, then run `truffle migrate` to deploy the contracts onto your network of choice (default "development").
+1. Then run `npm run dev` to build the app and serve it on http://localhost:8081
+
+
+
+
---
+++ app/javascripts/index.js
@@ -0,0 +1,31 @@
+var qrcode = new QRCode('qrcode')
+
+function makeCode () {
+  var elText = document.getElementById('text')
+  var contractAddress = document.getElementById('contractAddress')
+  var locationId = document.getElementById('locationId')
+  var locationName = document.getElementById('locationName')
+  var secret = document.getElementById('secret')
+  var passPhrase = document.getElementById('passPhrase')
+
+  var elText = contractAddress.value + locationName.value + locationId.value + secret.value + passPhrase.value
+  if (!elText) {
+    alert('Input a text')
+    elText.focus()
+    return
+  }
+
+  qrcode.makeCode(elText.value)
+}
+
+makeCode()
+
+$('#text')
+    .on('blur', function () {
+      makeCode()
+    })
+    .on('keydown', function (e) {
+      if (e.keyCode == 13) {
+        makeCode()
+      }
+    })
